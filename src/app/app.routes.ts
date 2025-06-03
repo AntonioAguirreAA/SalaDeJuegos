290c120d6f50d531a4f9fd993df10d4c7dd357fc
Antonio Aguirre <antiaguirre.aa@gmail.com>
2025-06-02 23:50:04 -0300

    login completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { JuegosComponent } from './componentes/juegos/juegos.component';
+import { QuiensoyComponent } from './componentes/quiensoy/quiensoy.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'juegos',
+    component: JuegosComponent,
+  },
+  {
+    path: 'quiensoy',
+    component: QuiensoyComponent,
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://bbdtxmrsanmjjhutysbj.supabase.co',
+  publicAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZHR4bXJzYW5tampodXR5c2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NzQ3MDMsImV4cCI6MjA2MzQ1MDcwM30.-8pOmT0LIp4jLavMBPVY5CwxFukkUEG_FxmS1CvBxhw',
+};
